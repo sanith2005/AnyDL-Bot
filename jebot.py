df7b6bae3f28c551f5ac03667082dbd38722c224
sanith2005 <77660641+sanith2005@users.noreply.github.com>
2021-04-01 07:15:54 -0700

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -14,33 +14,31 @@ Jebot = Client(
    bot_token=Config.TG_BOT_TOKEN,
 )
 
-YTDL_REGEX = (r"^((?:https?:)?\/\/)"
-              r"?((?:www|m)\.)"
-              r"?((?:youtube\.com|youtu\.be|xvideos\.com|pornhub\.com"
-              r"|xhamster\.com|xnxx\.com))"
-              r"(\/)([-a-zA-Z0-9()@:%_\+.~#?&//=]*)([\w\-]+)(\S+)?$")
-s2tw = OpenCC('s2tw.json').convert
-
 
 @Jebot.on_message(filters.command("start"))
 async def start(client, message):
    if message.chat.type == 'private':
        await Jebot.send_message(
                chat_id=message.chat.id,
-               text="""<b>Hey There, I'm AnyDL Bot
+               text="""<code>Hey There, I'm My K-Drama Bot
 
-I can download video or audio from Youtube. Made by @ImJanindu 🇱🇰
+I can download Korean TV series. Made by @sanithbimsara 🇱🇰
 
-Hit help button to find out more about how to use me</b>""",   
+Hit help button to find out more about how to use me</code>""",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[
                                         InlineKeyboardButton(
-                                            "Help", callback_data="help"),
-                                        InlineKeyboardButton(
-                                            "Channel", url="https://t.me/Infinity_BOTs")
+                                            "Update🔄", callback_data="update"),
+                                         InlineKeyboardButton(
+                                            "Help❓", callback_data="help"),
                                     ],[
                                       InlineKeyboardButton(
-                                            "Source Code", url="https://github.com/ImJanindu/AnyDL-Bot")
+                                            "Channel🗣", url="https://t.me/blackfoxprojects"),
+                                    ],[
+                                   InlineKeyboardButton(
+                                            "Request📩", url="http://t.me/r_e_q_u_e_s_t_bot"),
+                                         InlineKeyboardButton(
+                                            "More🛠", callback_data="more"),
                                     ]]
                             ),        
             disable_web_page_preview=True,        
@@ -51,20 +49,18 @@ async def help(client, message):
     if message.chat.type == 'private':   
         await Jebot.send_message(
                chat_id=message.chat.id,
-               text="""<b>AnyDL Bot Help!
-
-Just send a Youtube url to download it in video or audio format!
-
-~ @Infinity_BOTs</b>""",
+               text="""<b>My K-Drama Bot Help!</b>""",
         reply_markup=InlineKeyboardMarkup(
                                 [[
                                         InlineKeyboardButton(
-                                            "Back", callback_data="start"),
+                                            "Help❓", url="https://telegra.ph/My-K-Drama-Bot-Help-04-01"),
                                         InlineKeyboardButton(
                                             "About", callback_data="about"),
                                   ],[
                                         InlineKeyboardButton(
-                                            "Source Code", url="https://github.com/ImJanindu/AnyDL-Bot")
+                                            "More🛠", callback_data="more"),
+                                        InlineKeyboardButton(
+                                            "Back", callback_data="start"),
                                     ]]
                             ),        
             disable_web_page_preview=True,        
@@ -75,191 +71,71 @@ async def about(client, message):
     if message.chat.type == 'private':   
         await Jebot.send_message(
                chat_id=message.chat.id,
-               text="""<b>About AnyDL Bot!</b>
+               text="""<b>About My K-Drama Bot!</b>
 
-<b>♞ Developer:</b> <a href="https://t.me/ImJanindu">Janindu 🇱🇰</a>
+<b>💎 Developer:</b> <a href="https://t.me/sanithbimsara">Sanith 🇱🇰</a>
 
-<b>♞ Support:</b> <a href="https://t.me/InfinityBOTs_Support">Infinity BOTs Support</a>
+<b>💎 Support:</b> <a href="https://t.me/blackfoxprojects">Black Fox Projects</a>
 
-<b>♞ Library:</b> <a href="https://github.com/pyrogram/pyrogram">Pyrogram</a>
+<b>💎 Server:</b> <a href="https://heroku.com">Heroku</a>
 
-<b>~ @Infinity_BOTs</b>""",
+<b>💎 Request:</b> <a href="http://t.me/r_e_q_u_e_s_t_bot">Request Dramas</a>
+
+<b>💎 User Guide:</b> <a href="https://telegra.ph/My-K-Drama-Bot-Help-04-01">How to Use My K-Drama Bot</a>
+
+<b>💎 Language:</b> <a href="https://www.python.org/">Python</a>
+
+
+<b>~ @blackfoxprojects</b>""",
      reply_markup=InlineKeyboardMarkup(
                                 [[
                                         InlineKeyboardButton(
                                             "Back", callback_data="help"),
-                                        InlineKeyboardButton(
-                                            "Source Code", url="https://github.com/ImJanindu/AnyDL-Bot")
                                     ]]
                             ),        
             disable_web_page_preview=True,        
             parse_mode="html")
-
-
-# https://docs.pyrogram.org/start/examples/bot_keyboards
-# Reply with inline keyboard
-@Jebot.on_message(filters.private
-                   & filters.text
-                   & ~filters.edited
-                   & filters.regex(YTDL_REGEX))
-async def ytdl_with_button(_, message: Message):
-    await message.reply_text(
-        "**Choose download type 🤗**",
+      
+@Jebot.on_message(filters.command("update"))
+async def help(client, message):
+    if message.chat.type == 'private':   
+        await Jebot.send_message(
+               chat_id=message.chat.id,
+               text="""<b>My K-Drama Bot Was successfully Updated!✅</b>""",
         reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Audio 🎵",
-                        callback_data="ytdl_audio"
-                    ),
-                    InlineKeyboardButton(
-                        "Video 🎬",
-                        callback_data="ytdl_video"
-                    )
-                ]
-            ]
-        ),
-        quote=True
-    )
-
-
-@Jebot.on_callback_query(filters.regex("^ytdl_audio$"))
-async def callback_query_ytdl_audio(_, callback_query):
-    try:
-        url = callback_query.message.reply_to_message.text
-        ydl_opts = {
-            'format': 'bestaudio',
-            'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
-            'writethumbnail': True
-        }
-        with YoutubeDL(ydl_opts) as ydl:
-            message = callback_query.message
-            await message.reply_chat_action("typing")
-            info_dict = ydl.extract_info(url, download=False)
-            # download
-            await callback_query.edit_message_text("**Downloading audio...**")
-            ydl.process_info(info_dict)
-            # upload
-            audio_file = ydl.prepare_filename(info_dict)
-            task = asyncio.create_task(send_audio(message, info_dict,
-                                                  audio_file))
-            while not task.done():
-                await asyncio.sleep(3)
-                await message.reply_chat_action("upload_document")
-            await message.reply_chat_action("cancel")
-            await message.delete()
-    except Exception as e:
-        await message.reply_text(e)
-    await callback_query.message.reply_to_message.delete()
-    await callback_query.message.delete()
-
-
-async def send_audio(message: Message, info_dict, audio_file):
-    basename = audio_file.rsplit(".", 1)[-2]
-    # .webm -> .weba
-    if info_dict['ext'] == 'webm':
-        audio_file_weba = basename + ".weba"
-        os.rename(audio_file, audio_file_weba)
-        audio_file = audio_file_weba
-    # thumbnail
-    thumbnail_url = info_dict['thumbnail']
-    thumbnail_file = basename + "." + \
-        get_file_extension_from_url(thumbnail_url)
-    # info (s2tw)
-    webpage_url = info_dict['webpage_url']
-    title = '@Infinity_BOTs - '+s2tw(info_dict['title'])
-    caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
-    duration = int(float(info_dict['duration']))
-    performer = s2tw(info_dict['uploader'])
-    await message.reply_audio(audio_file, caption=caption, duration=duration,
-                              performer=performer, title=title,
-                              parse_mode='HTML', thumb=thumbnail_file)
-    os.remove(audio_file)
-    os.remove(thumbnail_file)
-
-
-@Jebot.on_callback_query(filters.regex("^ytdl_video$"))
-async def callback_query_ytdl_video(_, callback_query):
-    try:
-        # url = callback_query.message.text
-        url = callback_query.message.reply_to_message.text
-        ydl_opts = {
-            'format': 'best[ext=mp4]',
-            'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
-            'writethumbnail': True
-        }
-        with YoutubeDL(ydl_opts) as ydl:
-            message = callback_query.message
-            await message.reply_chat_action("typing")
-            info_dict = ydl.extract_info(url, download=False)
-            # download
-            await callback_query.edit_message_text("**Downloading video...**")
-            ydl.process_info(info_dict)
-            # upload
-            video_file = ydl.prepare_filename(info_dict)
-            task = asyncio.create_task(send_video(message, info_dict,
-                                                  video_file))
-            while not task.done():
-                await asyncio.sleep(3)
-                await message.reply_chat_action("upload_document")
-            await message.reply_chat_action("cancel")
-            await message.delete()
-    except Exception as e:
-        await message.reply_text(e)
-    await callback_query.message.reply_to_message.delete()
-    await callback_query.message.delete()
-
-
-async def send_video(message: Message, info_dict, video_file):
-    basename = video_file.rsplit(".", 1)[-2]
-    # thumbnail
-    thumbnail_url = info_dict['thumbnail']
-    thumbnail_file = basename + "." + \
-        get_file_extension_from_url(thumbnail_url)
-    # info (s2tw)
-    webpage_url = info_dict['webpage_url']
-    title = '@Infinity_BOTs - '+s2tw(info_dict['title'])
-    caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
-    duration = int(float(info_dict['duration']))
-    width, height = get_resolution(info_dict)
-    await message.reply_video(
-        video_file, caption=caption, duration=duration,
-        width=width, height=height, parse_mode='HTML',
-        thumb=thumbnail_file)
-
-    os.remove(video_file)
-    os.remove(thumbnail_file)
-
-
-def get_file_extension_from_url(url):
-    url_path = urlparse(url).path
-    basename = os.path.basename(url_path)
-    return basename.split(".")[-1]
-
-
-def get_resolution(info_dict):
-    if {"width", "height"} <= info_dict.keys():
-        width = int(info_dict['width'])
-        height = int(info_dict['height'])
-    # https://support.google.com/youtube/answer/6375112
-    elif info_dict['height'] == 1080:
-        width = 1920
-        height = 1080
-    elif info_dict['height'] == 720:
-        width = 1280
-        height = 720
-    elif info_dict['height'] == 480:
-        width = 854
-        height = 480
-    elif info_dict['height'] == 360:
-        width = 640
-        height = 360
-    elif info_dict['height'] == 240:
-        width = 426
-        height = 240
-    return (width, height)
-
-
+                                [[
+                                        InlineKeyboardButton(
+                                            "Back", callback_data="start"),
+                                    ]]
+                            ),        
+            disable_web_page_preview=True,        
+            parse_mode="html")
+      
+@Jebot.on_message(filters.command("more"))
+async def help(client, message):
+    if message.chat.type == 'private':   
+        await Jebot.send_message(
+               chat_id=message.chat.id,
+               text="""<b>More🛠</b>""",
+        reply_markup=InlineKeyboardMarkup(
+                                [[
+                                        InlineKeyboardButton(
+                                            "Rate⭐️⭐️", url="https://t.me/tlgrmcbot?start=mykdrama_bot-review"),
+                                         InlineKeyboardButton(
+                                            "Feedback🌀", url="http://t.me/r_e_q_u_e_s_t_bot"),
+                                  ],[
+                                        InlineKeyboardButton(
+                                            "Updates And notifications", url="https://telegra.ph/My-K-Drama-Bot-Update--Notification-04-01"),
+                                        InlineKeyboardButton(
+                                            "Dev", url="https://t.me/sanithbimsara"),
+                                ],[
+                                        InlineKeyboardButton(
+                                            "Back", callback_data="start"),
+                                    ]]
+                            ),        
+            disable_web_page_preview=True,        
+            parse_mode="html")
+      
 @Jebot.on_callback_query()
 async def button(bot, update):
       cb_data = update.data
@@ -272,11 +148,17 @@ async def button(bot, update):
       elif "start" in cb_data:
         await update.message.delete()
         await start(bot, update.message)
-
+      elif "update" in cb_data:
+        await update.message.delete()
+        await update(bot, update.message)
+       elif "more" in cb_data:
+        await update.message.delete()
+        await more(bot, update.message)
+         
 print(
     """
 Bot Started!
-Join @Infinity_BOTs
+Join @blackfoxprojects
 """
 )
 
